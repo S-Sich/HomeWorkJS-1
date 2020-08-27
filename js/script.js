45050dbc147c94dc41ecf1bca7953670a44de6d1
S-Sich <63017773+S-Sich@users.noreply.github.com>
2020-08-28 01:06:47 +0300

    lesson1

--- js/script.js
+++ js/script.js
@@ -1,8 +1,16 @@
 'use strict';
 
+let tempCelsius = prompt('Введите температуру в градусах по Цельсию', 20);
+
+let tempFahrenheit;
+tempFahrenheit = (9 / 5) * tempCelsius + 32;
+
+alert(tempFahrenheit);
+
 let admin;
 let name = 'Василий';
 admin = name;
 alert(admin);
 
-// Чему будет равно JS-выражение 1000 + "108"?  Ответ:'1000108'
+// Чему будет равно JS-выражение 1000 + "108"?
+alert('Ответ: 1000108')
